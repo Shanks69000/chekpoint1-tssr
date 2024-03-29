19d93b43ab66a2f9af2d628f6fc67b8fd3c457e7
Jarrets69000 <113185866+Shanks69000@users.noreply.github.com>
2024-03-29 12:52:57 +0100

    Add files via upload

---
+++ addUser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#creation d'utilisateur
+#si il y a des arguments alors on continue sinon afficher le probleme
+#si utilisateur existant afficher le probleme
+#sinon si creation du compte se passe bien alors afficher succes
+#sinon afficher probleme
+#fin
+
+if [ $# -eq 0 ]
+    then
+        echo "ajout d'argument"
+        exit 1
+        else
+            for user in $@
+            do
+                if cat /etc/passwd | grep "$user" &>/dev/null
+                    then
+                        echo "utilisater $user existe"
+                    else
+                        if adduser "$user"
+                            then
+                            echo "utilisateur $user a été cree"
+                        else
+                            echo "echec a la creation du $user"
+                        fi
+                fi
+            done
+fi
